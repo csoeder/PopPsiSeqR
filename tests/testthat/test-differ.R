7a673839521de704d68050a85dbef39942d18825
charles Soeder <csoeder@charless-Laptop.local>
2025-12-03 12:39:22 -0500

    minor updates; administrative bump

--- tests/testthat/test-differ.R
+++ tests/testthat/test-differ.R
@@ -2,3 +2,13 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 #under construction
+test_that("a thing is no different than itself",{
+
+  test.gr <-  data.frame(seqnames = "chr2L", start = 10000208, end=10000208, width =1, strand = "*", name = 0, score = 1, treatment = "placeholder" ) %>% GenomicRanges::GRanges()
+
+  expect_no_error(test.self.diff.gr <- subTractor(test.gr, test.gr, field = "score", treament_name = "treatment"))
+
+  expect_equal(test.self.diff.gr$placeholder_minus_placeholder[[1]], 0)
+
+})
+
